ded4331826dcebd0a3d7acb1f6253d1ce4312dac
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2024-04-17 23:28:48 +0300

    added movie controller and models

--- Materials/Примеры/1. ASP.NET Core application/SendFile/SendFile/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.Run(async (context) => await context.Response.SendFileAsync("Capri.jpg"));
-
-app.Run();
--- Materials/Примеры/1. ASP.NET Core application/WelcomePage/WelcomePage/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// WelcomePageMiddleware - компонент middlware-конвейера,
-// который отправляет клиенту некоторую стандартную веб-страницу
-app.UseWelcomePage();
-
-app.Run();
